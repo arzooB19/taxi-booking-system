a6df1861e34bcbdade64861fc44d2ed585770e7c
AdrianJJohnston <adjohnston@algomau.ca>
2025-03-25 21:51:57 -0400

    Adrian was here

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+//ADRIAN WAS HERE!!! (; //
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
